afb14851da156d13a710e6064a8bddbbce732a59
pontanan <datapontus@hotmail.com>
2017-08-22 21:45:46 +0200

    v0.3.0
    edit

--- Stone.cpp
+++ Stone.cpp
@@ -10,7 +10,7 @@ void Stone::setPosition(int x, int y)
     position = QRect(x, y, position.width(),position.height());
 }
 
-void Stone::update(MainCharacter *mc, Projectile *bullet, bool *killProjectile)
+void Stone::collision(MainCharacter *mc, Projectile *bullet, bool *killProjectile)
 {
     if( hitCheck(bullet) )
     {
@@ -22,23 +22,50 @@ void Stone::update(MainCharacter *mc, Projectile *bullet, bool *killProjectile)
         if(mc->isJumping)
             mc->isJumping = false;
 
-        if(mc->getDirection() == 0)
-            mc->setPosition(position.x() - mc->getWidth(), position.bottom() - (mc->getHeight() - 1));
-        else
-            mc->setPosition(position.x() + position.width(), position.bottom() - (mc->getHeight() - 1));
+        if(mc->getY() + mc->getHeight() >= position.y() &&  mc->getY() + mc->getHeight() <= position.y() + 10)
+        {
+            mc->setPosition(mc->getX(), position.y() - position.height());
+            mc->setYVelocity(0);
+        }
+        else if(mc->getDirection() == 0)
+            mc->setPosition(position.x() - mc->getWidth() - 2, position.bottom() - (mc->getHeight() - 1));
+        else if(mc->getDirection() == 1)
+            mc->setPosition(position.x() + position.width() + 2, position.bottom() - (mc->getHeight() - 1));
     }
 }
 
-void Stone::update(MainCharacter *mc)
+void Stone::collision(MainCharacter *mc)
 {
     if( hitCheck(mc) )
     {
         if(mc->isJumping)
             mc->isJumping = false;
 
-        if(mc->getDirection() == 0)
-            mc->setPosition(position.x() - mc->getWidth(), position.bottom() - (mc->getHeight() - 1));
-        else
-            mc->setPosition(position.x() + position.width(), position.bottom() - (mc->getHeight() - 1));
+        if(mc->getY() + mc->getHeight() >= position.y() &&  mc->getY() + mc->getHeight() <= position.y() + 10)
+        {
+            mc->setPosition(mc->getX(), position.y() - position.height());
+            mc->setYVelocity(0);
+        }
+        else if(mc->getDirection() == 0)
+            mc->setPosition(position.x() - mc->getWidth() - 2, position.bottom() - (mc->getHeight() - 1));
+        else if(mc->getDirection() == 1)
+            mc->setPosition(position.x() + position.width() + 2, position.bottom() - (mc->getHeight() - 1));
     }
+}
+
+void Stone::collision(Weapon *weapon)
+{
+    if( hitCheck(weapon) )
+    {
+
+        if(weapon->getDirection() == 0)
+            weapon->setPosition(position.x() - weapon->getWidth(), weapon->getY());
+        else
+            weapon->setPosition(position.x() + position.width() + 1, weapon->getY());
+    }
+}
+
+void Stone::update()
+{
+
 }
