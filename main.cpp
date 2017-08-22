5a33001693d7fa9100ccaf087e3361e09470e49e
pontanan <datapontus@hotmail.com>
2017-08-22 21:51:31 +0200

    v0.3.0
    edit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "gamewindow.h"
+#include "menuwindow.h"
 #include <QApplication>
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    GameWindow w;
+    MenuWindow w;
     w.show();
 
     return a.exec();
